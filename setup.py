76f0518dab3b6b55c0ded83ac2f4f51f8038de1e
Vicente Pinheiro <vpp.filho@gmail.com>
2022-03-19 11:17:11 -0300

    package dir

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='docker-images',
     version='0.1.0',
     description='Imagens Docker',
-    url='git@github.com:upgrade-php/docker-images.git',
+    url='https://github.com/upgrade-php/docker-images.git',
     author='Vicente Pinheiro',
     author_email='vpp.filho@gmail.com',
     license='unlicense',
